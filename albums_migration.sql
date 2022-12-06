a19e0a9d299c9643796079030e39eda7dbbb933a
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-12-06 15:13:59 -0600

    completed exercise

---
+++ albums_migration.sql
@@ -0,0 +1,22 @@
+
+USE codeup_test_db;
+
+SHOW databases;
+
+SHOW TABLES;
+
+DESCRIBE albums;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums
+(
+    id           INT UNSIGNED AUTO_INCREMENT,
+    artist       VARCHAR(200) DEFAULT 'Unknown'  NOT NULL,
+    name         VARCHAR(200) DEFAULT 'Untitled' NOT NULL,
+    release_date INT,
+    sales        FLOAT,
+    genre        VARCHAR(100),
+    PRIMARY KEY (id),
+    UNIQUE (name, artist)
+);
