783389fd7ce61b41477b64b42c3c7ff76665bd21
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-12-11 20:30:48 -0600

    completed aliases exercises

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -1,16 +1,8 @@
-
-SELECT hire_date, COUNT(*)
+SELECT CONCAT(emp_no, ' - ' ,last_name,' ',first_name)
+           AS full_name, birth_date AS DOB
 FROM employees
-GROUP BY hire_date
-ORDER BY COUNT(*) DESC
-LIMIT 10;
-
-SELECT hire_date, COUNT(*) as number_hired
-FROM employees
-GROUP BY hire_date
-ORDER BY number_hired DESC
-LIMIT 10;
-
+GROUP BY emp_no, last_name, first_name, birth_date
+limit 10;
 
 
 
