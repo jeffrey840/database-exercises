042ffd72087cdc56fb2f4a0b1ef41a6128027f75
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-12-13 21:20:59 -0600

    added relatiosdhips notes

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -1,5 +1,5 @@
 SELECT CONCAT(emp_no, ' - ' ,last_name,' ',first_name)
-           AS full_name, birth_date AS DOB
+AS full_name, birth_date AS DOB
 FROM employees
 GROUP BY emp_no, last_name, first_name, birth_date
 limit 10;
